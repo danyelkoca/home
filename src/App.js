81e99c97c9462b385791f1dcaa6d77ab27e908fd
danyelkoca <34621907+danyelkoca@users.noreply.github.com>
2021-09-03 00:45:41 +0900

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import profile from "./profile.jpeg";
 
+let hovers = Array.from(Array(144).keys()).map(function (a) {
+  return { a: false };
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        minHeight: "100vh",
+        maxHeight: "100vh",
+        height: "100vh",
+        minWidth: "100vw",
+        maxWidth: "100vw",
+        width: "100vw",
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      {/* <img src={profile} alt="profile" className="profile" /> */}
+      {Array.from(Array(144).keys()).map(function (a) {
+        let r = Math.random();
+        let c =
+          r < 0.25
+            ? "up"
+            : r >= 0.25 && r < 0.5
+            ? "down"
+            : r >= 0.5 && r < 0.75
+            ? "left"
+            : "right";
+
+        return (
+          <div
+            style={{
+              position: "relative",
+              width: window ? window.innerWidth / 12 : 100,
+              height: window ? window.innerWidth / 12 : 100,
+              backgroundColor: `hsl(${Math.floor(r * 36) * 10}, 70%, 80%)`,
+              borderRadius: 300,
+            }}
+            className={`${c} circle`}
+          ></div>
+        );
+      })}
     </div>
   );
 }
